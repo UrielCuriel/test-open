f23ced6772ef15c78a95b504466acd769456b1de
Uriel Curiel <ucuriel@musicbattles.mx>
2020-12-11 18:37:47 -0600

    eject project

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Openpay, { createDeviceSessionId } from 'rn-openpay'
+import { Component } from 'react'
+import { View, StyleSheet, Text, StatusBar } from 'react-native'
+export default class App extends Component {
+	state = {
+		loading: false,
+	}
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	successToken = (response) => {
+		const deviceSessionId = createDeviceSessionId()
+		const token = response.id
+		this.setState(() => ({ loading: false }))
+		console.log(deviceSessionId)
+		console.log(token)
+
+		// Make the call to your server with your charge request
+	}
+
+	failToken = (response) => {
+		console.log('failToken', response)
+	}
+
+	render() {
+		const address = {
+			city: 'Querétaro',
+			country_code: 'MX',
+			postal_code: '76900',
+			line1: 'Av 5 de Febrero',
+			line2: 'Roble 207',
+			line3: 'Col Carrillo',
+			state: 'Queretaro',
+		}
+		return (
+			<Openpay
+				isProductionMode={false}
+				merchantId="m53wxf73sonprp7fptgf"
+				publicKey="pk_bed8e880b6374d718c6a037329d2ab68"
+				address={address}
+				successToken={this.successToken}
+				failToken={this.failToken}
+				loading={this.state.loading}
+			/>
+			// <View style={styles.container}>
+			// 	<Text>Open up App.js to start working on your app!</Text>
+			// 	<StatusBar style="auto" />
+			// </View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
